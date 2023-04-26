6d8042718fb6b9fcce2b4193b924fd1b27f5ee72
Marco Antonio <marcotonho38@gmail.com>
2023-04-26 13:05:19 -0300

    formantando documento

--- exerc2/myproject/src/main/java/com/marco/validador/Validador.java
+++ exerc2/myproject/src/main/java/com/marco/validador/Validador.java
@@ -4,13 +4,13 @@ public class Validador {
     private String cpf;
     private int idade;
     private String nome;
-    
+
     public Validador(String cpf, int idade, String nome) {
         this.cpf = cpf;
         this.idade = idade;
         this.nome = nome;
     }
-    
+
     public String getCpf() {
         return cpf;
     }
@@ -34,19 +34,19 @@ public class Validador {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     public static void validaCPF(String cpf) throws IllegalArgumentException {
         if (cpf.length() != 11 || !cpf.matches("\\d+")) {
             throw new IllegalArgumentException("CPF inválido");
         }
     }
-    
+
     public static void validaIdade(int idade) throws IllegalArgumentException {
         if (idade < 0 || idade > 120) {
             throw new IllegalArgumentException("Idade inválida");
         }
     }
-    
+
     public static void validaNome(String nome) throws IllegalArgumentException {
         if (nome.isEmpty()) {
             throw new IllegalArgumentException("Nome inválido");
